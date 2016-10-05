9e02f98e9680180f28c48abfbc7f0202b72a7a92
devin-web <devin.gir@gmail.com>
2016-10-05 20:25:50 +0000

    Day3 funcitonality done; server broadcasting messages received, while appending uuids

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const SocketServer = require('ws').Server;
+const uuid = require('node-uuid');
 
 // Set the port to 4000
 const PORT = 4000;
@@ -15,12 +16,26 @@ const server = express()
 // Create the WebSockets server
 const wss = new SocketServer({ server });
 
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    client.send(data);
+  });
+};
+
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
 wss.on('connection', (ws) => {
   console.log('Client connected');
 
+  ws.onmessage = function (event) {
+    var parsedJSON = JSON.parse( event.data );
+    // console.log( "Message from client:", parsedJSON )
+    // console.log( "or", event.data );
+    //console.log( "User", parsedJSON.username, "said", parsedJSON.content, "uuid:", uuid.v4() );
+    wss.broadcast( JSON.stringify( { id: uuid.v4(), username: parsedJSON.username, content: parsedJSON.content } ) );
+  }
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
   ws.on('close', () => console.log('Client disconnected'));
 });
+
